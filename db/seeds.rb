dbfd50de8e02f05bea8b9f06c7801a7d89d6ded0
Keeley Mahoney <keeleymahoney@Keeleys-MacBook-Air.local>
2024-09-09 11:25:57 -0500

    added in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create!([
+    {title: "The Life of Pi"},
+    {title: "Holes"},
+    {title: "Normal People"},
+    {title: "Kulti"},
+    {title: "Gone with the Wind"},
+])
